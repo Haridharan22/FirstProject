ca37ab39dcfbc8aaa2b4d360b3a0978eee5fea1b
Haridharan22 <dharanhari2207@gmail.com>
2022-11-15 18:16:32 +0530

    react-quary

---
+++ query/RQsuperHeroespage.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { useQuery } from 'react-query'
+import axios from 'axios'
+
+export const RQsuperHeroespage = () => {
+  const {isLoading, data}= useQuery('super-heroes',()=>{
+    return axios.get('http://localhost:4000/superheroes')
+  })
+  if(isLoading){
+    return <h2>Loading....</h2>
+  }
+  return (
+    <div>
+      <h2>RQsuperHeroespage</h2>
+      {data?.data.map((hero)=>{
+        return <div key={hero.name}>{hero.name}</div>
+      })}
+    </div>
+  )
+}
